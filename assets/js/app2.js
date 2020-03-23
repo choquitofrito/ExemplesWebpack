fcbecd719b5d6a968db210ab28553e1e15d72b0c
choquitofrito <zyriab@gmail.com>
2020-03-23 22:48:09 +0100

    ok app and app2, after compile

---
+++ assets/js/app2.js
@@ -0,0 +1,13 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+
+// juste pour essayer une autre entry
+import '../css/app2.css';
+
+console.log ("une autre entry!");
